ed354c1fd5cf020c6db074235cb14b0711890e6e
jang walia <jangiwalia83@gmail.com>
2022-02-22 18:45:37 -0800

    refactor danger class to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,7 +8,7 @@ export default function Button(props) {
    if (props.confirm) {
      buttonClass += " button--confirm";
    }if(props.danger){
-      buttonClass += "button-- danger";
+      buttonClass += " button--danger";
    }
   return(
    <button
