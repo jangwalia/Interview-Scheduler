81344e98efa61a234e83bc61d2a9ebb6f5743af6
jang walia <jangiwalia83@gmail.com>
2022-03-02 11:21:02 -0800

    add unit test for selecters and add helper functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,74 +1,33 @@
 import "components/Application.scss";
 import DayList from "./DayList";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from "axios";
 import Appointment from "./Appointment/index";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+
 export default function Application(props) {
   const [day,setDay] = useState("Monday")
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
-  const schedule = Object.values(appointments).map(appointment => {
-    return(
-      <Appointment
-      key={appointment.id}
-      {...appointment}
-    />
-     
-    )
-  });
+  const [days,setDays] = useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:8001/api/days').then(response =>{
+      setDays([...response.data]);
+  })
+  },[])
+
+  useEffect(()=>{
+    axios.get('http://localhost:8001/api/appointments').then(appointments =>{
+      Object.values(appointments).map(appointment => {
+        return(
+          <Appointment
+          key={appointment.id}
+          {...appointment}
+        />
+         
+        )
+      });
+    })
+  },[day])
+  
   
 return (
     <main className="layout">
@@ -93,7 +52,7 @@ return (
 />
 </section>
 <section className="schedule">
-        {schedule}
+        
         <Appointment key="last" time="5pm" />
       </section>
 </main>
